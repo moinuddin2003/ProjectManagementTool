f2b7be16999079efc60880ef9133c2c31bd88101
Muhammad Moinudddin <moinuddin010203@gmail.com>
2025-08-12 00:28:55 +0500

    TAILWIND PROBLEM SOLVED

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // ✅ Adjust this if your files are in src/
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
